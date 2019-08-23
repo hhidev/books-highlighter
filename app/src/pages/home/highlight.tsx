28a45ac3b58df6e7809a7dfbe5dba9ae4081cbad
hhidev <hidenori.69ch@gmail.com>
2019-08-23 09:43:40 +0900

    add: ハイライトポップメニュー

--- app/src/pages/home/highlight.tsx
+++ app/src/pages/home/highlight.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { db } from '../../firebase';
+import styled from 'styled-components';
 
 interface Props {
   bookId: string;
@@ -11,9 +12,30 @@ interface Image {
   text: string;
 }
 
+interface SelectRect {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+  bottom: number;
+  top: number;
+  left: number;
+  right: number;
+}
+
 const Highlight: React.FunctionComponent<Props> = props => {
   const [images, setImages] = React.useState<Image[]>([]);
   const [selectedTabName, setSelectTab] = React.useState('ハイライト');
+  const [selectedText, setSelectedText] = React.useState('');
+  const [selectionRect, setSelectionRect] = React.useState<ClientRect>({
+    width: 0,
+    height: 0,
+    bottom: 0,
+    top: 0,
+    left: 0,
+    right: 0
+  });
+  console.log(selectionRect);
 
   React.useEffect(() => {
     if (props.bookId) {
@@ -33,6 +55,26 @@ const Highlight: React.FunctionComponent<Props> = props => {
   }, [props.bookId]);
 
   const showOnMenu = () => {
+    if (window.getSelection().type === 'Range') {
+      setSelectedText(window.getSelection().toString());
+      setSelectionRect(
+        window
+          .getSelection()
+          .getRangeAt(0)
+          .getBoundingClientRect()
+      );
+    } else {
+      setSelectedText('');
+      setSelectionRect({
+        width: 0,
+        height: 0,
+        bottom: 0,
+        top: 0,
+        left: 0,
+        right: 0
+      });
+    }
+
     console.log(window.getSelection().toString());
     console.log(
       window
@@ -60,13 +102,28 @@ const Highlight: React.FunctionComponent<Props> = props => {
           </li>
         </ul>
       </div>
+
+      {/*TODO スクロール量を加算する */}
+      <PopupMenu style={{ left: '0px', top: `${selectionRect.top - 24}px` }}>
+        <ButtonContainer
+          style={{
+            transform: 'translateX(-33.9741%)',
+            left: `${selectionRect.left}px`
+          }}
+        >
+          <ColorButton color={'yellow'} />
+          <ColorButton color={'blue'} />
+          <ColorButton color={'red'} />
+          <a>ハイライト</a>
+          <a>編集</a>
+        </ButtonContainer>
+        <Triangle left={selectionRect.left} />
+      </PopupMenu>
+
       {selectedTabName === '未処理データ' &&
         images.map((image, i) => {
           return (
             <div key={i} style={{ marginBottom: '1em' }}>
-              <div className={'selection'}>
-                <div>ハイライトする</div>
-              </div>
               <pre onMouseUp={showOnMenu}>{image.text}</pre>
             </div>
           );
@@ -74,5 +131,49 @@ const Highlight: React.FunctionComponent<Props> = props => {
     </React.Fragment>
   );
 };
+
+const PopupMenu = styled('div')`
+  position: absolute;
+  left: 0;
+  width: 100%;
+  z-index: 300;
+  transform: translateY(calc(-100% - 24px));
+  -webkit-user-select: none;
+  user-select: none;
+  pointer-events: none;
+`;
+
+const ButtonContainer = styled('a')`
+  position: relative;
+  display: inline-block;
+  width: 250px;
+  text-align: center;
+  background-color: #111;
+  padding: 0 1px;
+  border-radius: 4px;
+  pointer-events: auto;
+  padding: 0.7em;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+const Triangle = styled('div')<{ left: number }>`
+  left: ${props => props.left + 24}px;
+  position: absolute;
+  transform: translateX(-50%);
+  width: 0;
+  height: 0;
+  border-top: 6px solid #111;
+  border-right: 8px solid transparent;
+  border-left: 8px solid transparent;
+`;
+
+const ColorButton = styled('a')<{ color: string }>`
+  background-color: ${props => props.color};
+  width: 24px;
+  height: 24px;
+  border-radius: 50%;
+`;
 
 export default Highlight;
